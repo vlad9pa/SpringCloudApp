191df7c59752bdf3d23f680369ae046e4e176783
vlad9pa <vlad.milytin@gmail.com>
2017-07-08 15:03:44 +0300

    maybe right way to secure services??

--- user-client/src/main/java/com/vlad9pa/springcloud/UserServiceClientApplication.java
+++ user-client/src/main/java/com/vlad9pa/springcloud/UserServiceClientApplication.java
@@ -3,11 +3,8 @@ package com.vlad9pa.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -16,7 +13,6 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableZuulProxy
 @EnableCircuitBreaker
 @EnableAutoConfiguration
-@EnableOAuth2Sso
 @ComponentScan("com.vlad9pa.springcloud")
 public class UserServiceClientApplication {
 
--- user-client/src/main/java/com/vlad9pa/springcloud/config/AuthorizationServerConfig.java
+++ user-client/src/main/java/com/vlad9pa/springcloud/config/AuthorizationServerConfig.java
@@ -8,17 +8,25 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 /**
  * @author Vlad Milyutin.
  */
 @Configuration
 @EnableAuthorizationServer
+@SessionAttributes("authorizationRequest")
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
     private final AuthenticationManager authenticationManager;
+
+    @Autowired
+    public AuthorizationServerConfig(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
@@ -28,12 +36,15 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
-                .withClient("browser")
-                .secret("browser")
-                .scopes("read", "write")
-                .authorizedGrantTypes("password")
+                .withClient("client")
+                    .secret("clientpassword")
+                    .scopes("openid")
+                    .authorizedGrantTypes("authorization_code", "refresh_token","password")
+                .and()
+                .withClient("resource-server")
+                    .secret("browser")
+                    .authorizedGrantTypes("implicit")
                 .accessTokenValiditySeconds(3600);
-
     }
 
     @Bean
@@ -42,4 +53,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     }
 
 
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+       security.tokenKeyAccess("permitAll()").checkTokenAccess(
+               "isAuthenticated()");
+    }
 }
